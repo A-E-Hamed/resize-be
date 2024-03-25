1f768df3e1a21d3e3bfd7bbb2c9c3bd9366a3c23
Ahmed-S-Hamed <elidyan2010s@gmail.com>
2024-03-25 12:32:26 +0200

    cors fix

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const { resizeMedia } = require('./controllers/resizeController');
-
 const app = express();
 const port = 4800;
-
 // Middleware to handle file uploads
 app.use(fileUpload({
     createParentPath: true
 }));
-
+// Manual CORS Headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allows all origins
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 // Route to upload and resize media
 app.post('/api/upload', async (req, res) => {
     try {
@@ -19,5 +26,4 @@ app.post('/api/upload', async (req, res) => {
         res.status(500).send('Server error occurred.');
     }
 });
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
